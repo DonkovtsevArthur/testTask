4dbebf493427450e310591ad72cf51150c7c57c6
Donkovtsev Arthur <janeair-20@mail.ru>
2018-05-08 16:55:14 +0300

    добавил кнопку выход, осталось дописать логику

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, withRouter, Link } from "react-router-dom";
+
+import { connect } from "react-redux";
 
 
 const styleLink = { listStyleType: "none", display: 'inline-block', marginLeft: '10px'  };
 
-const Menu = () => {
+const Menu = ({isOpen, onOut}) => {
   return <ul>
       <li style={styleLink}>
         <NavLink exact to="/">
@@ -17,10 +19,22 @@ const Menu = () => {
       <li style={styleLink}>
         <NavLink to="/profile">Profile</NavLink>
       </li>
-      
+      {isOpen ? <li style={styleLink}>
+          <Link onClick={() => onOut()} to="/login">
+            Выход
+          </Link>
+        </li> : ""}
     </ul>;
 };
 
+const mapStateProps = state => ({
+  isOpen : state.getLogin.isOpen
+})
 
+const mapDispatchProps = dispatch => ({
+  onOut: () => {
+    dispatch({ type: "OUT_IN_LOGIN" });
+  }
+});
 
-export default Menu;
+export default withRouter(connect(mapStateProps, mapDispatchProps)(Menu));
